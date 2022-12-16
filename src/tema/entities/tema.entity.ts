d113010dd1a35c06e53b89d348dde3aabab35875
Carolina Tavares <93790048+anacahrowl@users.noreply.github.com>
2022-12-16 15:30:28 -0300

    Update tema.entity.ts

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,30 +1,23 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 import { type } from "os";
+
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
-import { Usuario } from "../../usuario/entities/usuario.entity";
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({name: "tb_tema"})
-    export class Tema{
+@Entity({name: "tb_temas"})
+export class Tema {
 
-        @PrimaryGeneratedColumn('increment')
-        @ApiProperty()
-        id: number
-        
-        @IsNotEmpty()
-        @Column({length: 100, nullable: false})
-        @ApiProperty()
-        titulo: string
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+    id: number
 
-        @IsNotEmpty()
-        @Column({length: 100, nullable: false})
-        categoria: string
+    @IsNotEmpty()
+    @Column({length: 255, nullable:false})
+    @ApiProperty()
+    descricao: string 
 
-        @ApiProperty({type: () => Postagem})
-
-        @OneToMany( () => Postagem, (postagem) => postagem.tema)
-        postagem: Postagem []
-
-        
- }
+    @ApiProperty({type: () => Postagem})
+    @OneToMany(() => Postagem, (postagem) => postagem.tema)
+    postagem: Postagem[]
+}
