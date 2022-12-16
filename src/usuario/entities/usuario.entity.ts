f81e56470436359463aab56930fff177a4e841a2
Carolina Tavares <93790048+anacahrowl@users.noreply.github.com>
2022-12-16 15:53:37 -0300

    Update usuario.entity.ts

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -5,44 +5,36 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
 
 @Entity({name: "tb_usuarios"})
-
 export class Usuario {
 
     @PrimaryGeneratedColumn()
     @ApiProperty()
-
     id: number
 
 
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
     @ApiProperty()
-
     nome: string
 
     @IsEmail()
     @Column({length: 255, nullable: false})
-    @ApiProperty({ example: "usuario@gmail.com",})
-
-    usuario: string 
+    @ApiProperty({ example: "email@email.com.br"})
+    usuario: string //e-mail
 
     @IsNotEmpty()
     @MinLength(8)
     @Column({length: 255, nullable: false})
     @ApiProperty()
-
     senha: string
 
     @Column({length: 5000})
     @ApiProperty()
-
     foto: string
 
     @ApiProperty({ type: () => Postagem})
-
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
     postagem: Postagem[]
-    tema: any;
 
 
 }
