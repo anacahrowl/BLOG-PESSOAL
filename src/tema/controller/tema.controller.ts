18796dd60b01a89bcf844e4b66761043597319a0
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-16 18:49:36 -0300

    Update tema.controller.ts

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards } 
 import { HttpStatus } from "@nestjs/common/enums";
 import { ParseIntPipe } from "@nestjs/common/pipes";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger/dist";
-import { JwtAuthGuard } from "../../auth/guard/jwt.auth.guard";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { Tema } from "../entities/tema.entity";
 import { TemaService } from "../service/tema.service";
 
